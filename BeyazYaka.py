b067d08e626561f2760bbb4945041706878ffdf7
melisaerdem <100697933+melisaerdem@users.noreply.github.com>
2023-06-09 01:46:26 +0300

    beyazyaka class tanim

---
+++ BeyazYaka.py
@@ -0,0 +1,4 @@
+from Calisan import Calisan
+class BeyazYaka(Calisan):
+    def __init__(self, tc_no, ad, soyad, yas, cinsiyet, uyruk, sektor, tecrube, maas, tesvik_primi):
+        super().__init__(tc_no, ad, soyad, yas, cinsiyet, uyruk, sektor, tecrube, maas)
