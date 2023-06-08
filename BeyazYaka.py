6031f9f62c1705c18336fd6599eaa948a59548f4
melisaerdem <100697933+melisaerdem@users.noreply.github.com>
2023-06-09 01:49:24 +0300

    zam hakki hesap

--- BeyazYaka.py
+++ BeyazYaka.py
@@ -8,3 +8,18 @@ class BeyazYaka(Calisan):
     
     def set_tesvik_primi(self, tesvik_primi):
         self.__tesvik_primi = tesvik_primi
+    def zam_hakki(self):
+        try:
+            tecrube_yil = self.get_tecrube() / 12
+            maas_oran = self.get_maas() / tecrube_yil
+            
+            if tecrube_yil < 2:
+                return self.__tesvik_primi
+            elif tecrube_yil >= 2 and tecrube_yil <= 4 and self.get_maas() < 15000:
+                return (self.get_maas() / tecrube_yil) * 5 + self.__tesvik_primi
+            elif tecrube_yil > 4 and self.get_maas() < 25000:
+                return (self.get_maas() / tecrube_yil) * 4 + self.__tesvik_primi
+            else:
+                return self.get_maas()
+        except:
+            return self.get_maas()
