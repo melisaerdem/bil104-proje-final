40d94f4a7029be3b323179f8089c0faf2434e82e
melisaerdem <100697933+melisaerdem@users.noreply.github.com>
2023-06-09 01:41:42 +0300

    get set

--- Issiz.py
+++ Issiz.py
@@ -3,3 +3,8 @@ class Issiz(Insan):
     def __init__(self, tc_no, ad, soyad, yas, cinsiyet, uyruk, statuler):
         super().__init__(tc_no, ad, soyad, yas, cinsiyet, uyruk)
         self.__statuler = statuler
+    def get_statuler(self):
+        return self.__statuler
+    
+    def set_statuler(self, statuler):
+        self.__statuler = statuler
