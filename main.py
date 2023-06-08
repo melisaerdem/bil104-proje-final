7addb8f9f9e09e588d943f788696195c4dfb1d88
melisaerdem <100697933+melisaerdem@users.noreply.github.com>
2023-06-09 01:52:48 +0300

    dataFrame

--- main.py
+++ main.py
@@ -20,3 +20,10 @@ mavi_yaka2 = MaviYaka("98765432109", "Ayşe", "Demir", 25, "Kadın", "Türk", "P
 # BeyazYaka sınıfı için 2 nesne oluşturulması
 beyaz_yaka1 = BeyazYaka("12345678901", "Ali", "Yılmaz", 30, "Erkek", "Türk", "Bilişim", 24, 12000, 1000)
 beyaz_yaka2 = BeyazYaka("98765432109", "Ayşe", "Demir", 25, "Kadın", "Türk", "Pazarlama", 36, 18000, 1500)
+# Verilerin bir DataFrame'e aktarılması
+data = [
+    [insan1.get_tc_no(), insan1.get_ad(), insan1.get_soyad(), insan1.get_yas(), insan1.get_cinsiyet(), insan1.get_uyruk()],
+    [insan2.get_tc_no(), insan2.get_ad(), insan2.get_soyad(), insan2.get_yas(), insan2.get_cinsiyet(), insan2.get_uyruk()],
+    [issiz1.get_tc_no(), issiz1.get_ad(), issiz1.get_soyad(), issiz1.get_yas(), issiz1.get_cinsiyet(), issiz1.get_uyruk()],
+    [issiz2.get_tc_no(), issiz2.get_ad(), issiz2.get_soyad(), issiz2.get_yas(), issiz2.get_cinsiyet(), issiz2.get_uyruk()],
+    [issiz3.get_tc_no(), issiz3.get_ad(), issiz3.get_soyad(), issiz3.get_yas(), issiz3.get_cinsiyet(), issiz3.get_uyruk()],
