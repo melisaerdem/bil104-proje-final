ac6ceff1d0d826d60a1fa242bd1ca2cba3bfa2fe
melisaerdem <100697933+melisaerdem@users.noreply.github.com>
2023-06-09 01:51:02 +0300

    issiz sinifi 3nesne

--- main.py
+++ main.py
@@ -7,3 +7,7 @@ import pandas as pd
 # Insan sınıfı için 2 nesne oluşturulması
 insan1 = Insan("12345678901", "Ali", "Yılmaz", 30, "Erkek", "Türk")
 insan2 = Insan("98765432109", "Ayşe", "Demir", 25, "Kadın", "Türk")
+# Issiz sınıfı için 3 nesne oluşturulması
+issiz1 = Issiz("12345678901", "Ali", "Yılmaz", 30, "Erkek", "Türk", {"mavi yaka": 2, "beyaz yaka": 1, "yonetici": 0})
+issiz2 = Issiz("98765432109", "Ayşe", "Demir", 25, "Kadın", "Türk", {"mavi yaka": 0, "beyaz yaka": 3, "yonetici": 1})
+issiz3 = Issiz("45678912345", "Mehmet", "Kara", 35, "Erkek", "Türk", {"mavi yaka": 0, "beyaz yaka": 0, "yonetici": 0})
