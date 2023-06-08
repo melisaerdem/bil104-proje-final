b8dbf738ab559e7a66c38ff187ccca189ec6fd0a
melisaerdem <100697933+melisaerdem@users.noreply.github.com>
2023-06-09 01:50:21 +0300

    insan sinifi 2 nesne

--- main.py
+++ main.py
@@ -4,3 +4,6 @@ from Calisan import Calisan
 from MaviYaka import MaviYaka
 from BeyazYaka import BeyazYaka
 import pandas as pd
+# Insan sınıfı için 2 nesne oluşturulması
+insan1 = Insan("12345678901", "Ali", "Yılmaz", 30, "Erkek", "Türk")
+insan2 = Insan("98765432109", "Ayşe", "Demir", 25, "Kadın", "Türk")
