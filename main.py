10e3a13f3d92a6a1db2ea1dd6264ab5596a6ae0a
melisaerdem <100697933+melisaerdem@users.noreply.github.com>
2023-06-09 01:54:38 +0300

    calisan mavi yaka beyaz yaka maas ortalamalarini yazdir

--- main.py
+++ main.py
@@ -37,3 +37,7 @@ data = [
 columns = ['TC No', 'Ad', 'Soyad', 'Yaş', 'Cinsiyet', 'Uyruk', 'Sektör', 'Tecrübe', 'Maaş', 'Yıpranma Payı/Tesvik Prim','Yeni Maaş']
 df = pd.DataFrame(data, columns=columns)
 df.fillna(0, inplace=True)
+#Çalışan,mavi yaka ve beyaz yaka için gruplandırarak tecrübe ve yeni maaş ortalamalarını her grup için yazdır#
+grouped = df.groupby('Sektör')[['Tecrübe', 'Maaş']].mean()
+print("\n---- Gruplanmış Veriler ----")
+print(grouped)
